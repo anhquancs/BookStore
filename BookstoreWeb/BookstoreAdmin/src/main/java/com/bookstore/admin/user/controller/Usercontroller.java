876dc252be36402a6dfe1e360a80f536d0bd6513
Your Name <you@example.com>
2024-06-03 04:26:43 +0700

    seach users

--- BookstoreWeb/BookstoreAdmin/src/main/java/com/bookstore/admin/user/controller/Usercontroller.java
+++ BookstoreWeb/BookstoreAdmin/src/main/java/com/bookstore/admin/user/controller/Usercontroller.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -30,25 +31,42 @@ public class Usercontroller {
 
 	@GetMapping("/users")
 	public String listFirstPage(Model model) {
-		return listByPage(1, model);
+		return listByPage(1, model, "firstName", "asc", null);
 	}
 
+
 	@GetMapping("/users/page/{pageNum}")
-	public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model) {
-		Page<User> page = service.listByPage(pageNum);
-		List<User> listUsers = page.getContent();
+	public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model, 
+			@Param("sortField") String sortField, 
+			@Param("sortDir") String sortDir, 
+			@Param("keyword") String keyword) {
+		
+		System.out.println("Sort Field: "+ sortField);
+		System.out.println("Sort Order: "+ sortDir);
+
+		Page<User> page = service.listByPage(pageNum, sortField, sortDir, keyword);
+		List<User> listUsers = page.getContent(); 
 
 		long startCount = (pageNum - 1) * UserService.USER_PER_PAGE + 1;
-		long endCount = startCount + UserService.USER_PER_PAGE - 1;
+		long endCount = startCount + UserService.USER_PER_PAGE - 1; 
 		if (endCount > page.getTotalElements()) {
-			endCount = page.getTotalElements();
+			endCount = page.getTotalElements(); 
 		}
-		model.addAttribute("currentPage", pageNum);
-		model.addAttribute("totalPages", page.getTotalPages());
-		model.addAttribute("startCount", startCount);
-		model.addAttribute("endCount", endCount);
-		model.addAttribute("totalItems", page.getTotalElements());
+		
+		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+
+		model.addAttribute("currentPage", pageNum); 
+		model.addAttribute("totalPages", page.getTotalPages()); 
+
+		model.addAttribute("startCount", startCount); 
+		model.addAttribute("endCount", endCount); 
+		model.addAttribute("totalItems", page.getTotalElements()); 
 		model.addAttribute("listUsers", listUsers);
+		model.addAttribute("sortField", sortField);
+		model.addAttribute("sortDir", sortDir);
+		model.addAttribute("reverseSortDir", reverseSortDir);
+		model.addAttribute("keyword", keyword);
+
 
 		return "users";
 	}
