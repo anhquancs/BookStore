30c83ca42bd25daf0936eefc067ca7edde8daf97
Your Name <you@example.com>
2024-06-18 21:56:40 +0700

    fix login

--- BookstoreWeb/BookstoreAdmin/src/main/java/com/bookstore/admin/security/WebSecurityConfig.java
+++ BookstoreWeb/BookstoreAdmin/src/main/java/com/bookstore/admin/security/WebSecurityConfig.java
@@ -1,46 +1,60 @@
 package com.bookstore.admin.security;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.bookstore.admin.user.service.BookStoreUserDetailsService;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-	//extends WebSecurityConfigurationAdapter
 
 	@Bean
-    public PasswordEncoder PasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
+	UserDetailsService userDetailsService() {
+		return new BookStoreUserDetailsService();
+	}
+	
 	@Bean
-	SecurityFilterChain configureHttpSecurity(HttpSecurity http) throws Exception {
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
 		
-		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-		
-		return http.build();
+		return authProvider;
 	}
 
-	// @Override
-	// protected void configure(HttpSecurity http) throws Exception{
-	// 	http.authorizeRequests()
-	// 		.anyRequest().authenticated()
-	// 		.and()
-	// 		.formLogin()
-	// 			.loginPage("/login")
-	// 			.usernameParameter("email")
-	// 			.permitAll();
-	// }
+	@Bean
+	SecurityFilterChain configureHttp(HttpSecurity http) throws Exception {
+		http.authenticationProvider(authenticationProvider());
+		
+		http.authorizeHttpRequests(auth -> auth
+				.anyRequest().authenticated()
+			)
+			.formLogin(form -> form			
+				.loginPage("/login")
+				.usernameParameter("email")
+				.permitAll()
+			);
+			
+			return http.build();
+	}
 
-	// @Override
-	// public void configure(WebSecurity web) throws Exception{
-	// 	web.ignoring().antMatchers("/images/++", "/js/++", "webjars/++");
-	// }
-
+	@Bean
+	WebSecurityCustomizer configureWebSecurity() throws Exception {
+		return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
+	}
+	
 }
