b4df79c87d58428ffc9b8be21eefe5ba918d4746
thuy <tranthuy060423k3@gmail.com>
2024-06-18 19:43:34 +0700

    login

--- BookstoreWeb/BookstoreAdmin/src/main/java/com/bookstore/admin/security/WebSecurityConfig.java
+++ BookstoreWeb/BookstoreAdmin/src/main/java/com/bookstore/admin/security/WebSecurityConfig.java
@@ -4,14 +4,17 @@ package com.bookstore.admin.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
+	//extends WebSecurityConfigurationAdapter
 
-    @Bean
+	@Bean
     public PasswordEncoder PasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
@@ -23,4 +26,21 @@ public class WebSecurityConfig {
 		
 		return http.build();
 	}
+
+	// @Override
+	// protected void configure(HttpSecurity http) throws Exception{
+	// 	http.authorizeRequests()
+	// 		.anyRequest().authenticated()
+	// 		.and()
+	// 		.formLogin()
+	// 			.loginPage("/login")
+	// 			.usernameParameter("email")
+	// 			.permitAll();
+	// }
+
+	// @Override
+	// public void configure(WebSecurity web) throws Exception{
+	// 	web.ignoring().antMatchers("/images/++", "/js/++", "webjars/++");
+	// }
+
 }
