c9246f11e5e3dd6d19b572cce71626971b60452b
TranThuy64 <tranthuy060423k3@gmail.com>
2024-06-22 20:33:51 +0700

    Category

---
+++ BookstoreWeb/BookstoreAdmin/src/main/java/com/bookstore/admin/category/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.bookstore.admin.category;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.bookstore.entity.Category;
+
+public interface CategoryRepository extends PagingAndSortingRepository <Category, Integer> {
+
+    
+}
+
