59bfa28e0c1d82e2d133886d4b0bb604cf438133
Your Name <you@example.com>
2024-07-07 17:25:31 +0700

    debug exportCSV brands

--- BookstoreWeb/BookstoreAdmin/src/main/java/com/bookstore/admin/brand/BrandCSVExporter.java
+++ BookstoreWeb/BookstoreAdmin/src/main/java/com/bookstore/admin/brand/BrandCSVExporter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
 import org.supercsv.io.CsvBeanWriter;
 import org.supercsv.io.ICsvBeanWriter;
 import org.supercsv.prefs.CsvPreference;
@@ -12,7 +13,7 @@ import com.bookstore.admin.user.Abstract.Exporter;
 import com.bookstore.entity.Brand;
 
 import jakarta.servlet.http.HttpServletResponse;
-
+@Component
 public class BrandCSVExporter extends Exporter {
 	public void export(List<Brand> listBrands, HttpServletResponse response) throws IOException {
 		
