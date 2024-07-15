de77c74125416e07bcf59135e44d27a0a79e1e37
TranThuy64 <tranthuy060423k3@gmail.com>
2024-07-15 19:23:08 +0700

    City, District

---
+++ BookstoreModel/src/main/java/com/bookstore/entity/City.java
@@ -0,0 +1,76 @@
+package com.bookstore.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cities")
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 45)
+    private String name;
+
+    @Column(nullable = false, length = 5)
+    private String code;
+
+    @OneToMany(mappedBy = "city")
+    private Set<District> districts;
+
+    public City() {
+        
+    }
+
+    public City(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Set<District> getDistricts() {
+        return districts;
+    }
+
+    public void setDistricts(Set<District> districts) {
+        this.districts = districts;
+    }
+
+    @Override
+    public String toString() {
+        return "City [id=" + id + ", name=" + name + ", code=" + code + "]";
+    }
+
+    
+}
---
+++ BookstoreModel/src/main/java/com/bookstore/entity/District.java
@@ -0,0 +1,69 @@
+package com.bookstore.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "districts")
+public class District {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 45)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private City city;
+
+    public District() {
+
+    }
+
+    public District(String name, City city) {
+        this.name = name;
+        this.city = city;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "District [id=" + id + ", name=" + name + "]";
+    }
+
+    
+}
