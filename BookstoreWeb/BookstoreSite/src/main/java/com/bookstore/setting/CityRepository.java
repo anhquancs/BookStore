1853b11a463a72bbba8d58db8dc05fe1b098bf5c
khanh <khanhhnpd07078@fpt.edu.vn>
2024-07-21 14:01:39 +0700

    .

--- BookstoreWeb/BookstoreSite/src/main/java/com/bookstore/setting/CityRepository.java
+++ BookstoreWeb/BookstoreSite/src/main/java/com/bookstore/setting/CityRepository.java
@@ -2,6 +2,7 @@ package com.bookstore.setting;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.bookstore.entity.City;
