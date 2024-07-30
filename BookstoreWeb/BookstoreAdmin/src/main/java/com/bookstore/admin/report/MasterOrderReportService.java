bc593120e6ee0f2102f13f1fb0f7df7bf8e3df41
khanh <khanhhnpd07078@fpt.edu.vn>
2024-07-30 16:33:40 +0700

    report 2

--- BookstoreWeb/BookstoreAdmin/src/main/java/com/bookstore/admin/report/MasterOrderReportService.java
+++ BookstoreWeb/BookstoreAdmin/src/main/java/com/bookstore/admin/report/MasterOrderReportService.java
@@ -1,7 +1,5 @@
 package com.bookstore.admin.report;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -14,29 +12,24 @@ import com.bookstore.admin.order.OrderRepository;
 import com.bookstore.entity.order.Order;
 
 @Service
-public class MasterOrderReportService {
+public class MasterOrderReportService extends AbstractReportService {
 	@Autowired private OrderRepository repo;
-	private DateFormat dateFormatter;
+
 	
-	public List<ReportItem> getReportDataLast7Days() {
-		System.out.println("getReportDataLast7Days...");
-		return getReportDataLastXDays(7);
+	protected List<ReportItem> getReportDataByDateRangeInternal(Date startTime, Date endTime, ReportType reportType) {
+		List<Order> listOrders = repo.findByOrderTimeBetween(startTime, endTime);
+		printRawData(listOrders);
+		
+		List<ReportItem> listReportItems = createReportData(startTime, endTime, reportType);
+		
+		System.out.println();
+		
+		calculateSalesForReportData(listOrders, listReportItems);
+		
+		printReportData(listReportItems);
+		
+		return listReportItems;
 	}
-	
-	private List<ReportItem> getReportDataLastXDays(int days) {
-		Date endTime = new Date();
-		Calendar cal = Calendar.getInstance();
-		cal.add(Calendar.DAY_OF_MONTH, -(days - 1));
-		Date startTime = cal.getTime();
-		
-		System.out.println("Start time: " + startTime);
-		System.out.println("End time: " + endTime);
-		
-		dateFormatter = new SimpleDateFormat("yyy-MM-dd");
-		
-		return getReportDataByDateRange(startTime, endTime);
-	}
-	
 
 	private void calculateSalesForReportData(List<Order> listOrders, List<ReportItem> listReportItems) {
 		for (Order order : listOrders) {
@@ -64,7 +57,7 @@ public class MasterOrderReportService {
 		
 	}
 
-	private List<ReportItem> createReportData(Date startTime, Date endTime) {
+	private List<ReportItem> createReportData(Date startTime, Date endTime, ReportType reportType) {
 		List<ReportItem> listReportItems = new ArrayList<>();
 		
 		Calendar startDate = Calendar.getInstance();
@@ -79,7 +72,12 @@ public class MasterOrderReportService {
 		listReportItems.add(new ReportItem(dateString));
 		
 		do {
-			startDate.add(Calendar.DAY_OF_MONTH, 1);
+			if (reportType.equals(ReportType.DAY)) {
+				startDate.add(Calendar.DAY_OF_MONTH, 1);
+			} else if (reportType.equals(ReportType.MONTH)) {
+				startDate.add(Calendar.MONTH, 1);
+			}
+			
 			currentDate = startDate.getTime();
 			dateString = dateFormatter.format(currentDate);	
 			
@@ -97,81 +95,5 @@ public class MasterOrderReportService {
 		});
 	}
 
-    public List<ReportItem> getReportDataLast28Days() {
-		return getReportDataLastXDays(28);
-	}
-
-    public List<ReportItem> getReportDataLast6Months() {
-		return getReportDataLastXMonths(6);
-	}
-
-    private List<ReportItem> getReportDataLastXMonths(int months) {
-		Date endTime = new Date();
-		Calendar cal = Calendar.getInstance();
-		cal.add(Calendar.MONTH, -(months - 1));
-		Date startTime = cal.getTime();
-		
-		System.out.println("Start time: " + startTime);
-		System.out.println("End time: " + endTime);
-		
-		dateFormatter = new SimpleDateFormat("yyyy-MM");
-		
-		return getReportDataByDateRange(startTime, endTime, "months");
-	}	
-
-
-	public List<ReportItem> getReportDataByDateRange(Date startTime, Date endTime) {
-		dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
-		return getReportDataByDateRange(startTime, endTime, "days");
-	}
 	
-	private List<ReportItem> getReportDataByDateRange(Date startTime, Date endTime, String period) {
-		List<Order> listOrders = repo.findByOrderTimeBetween(startTime, endTime);
-		printRawData(listOrders);
-		
-		List<ReportItem> listReportItems = createReportData(startTime, endTime, period);
-		
-		System.out.println();
-		
-		calculateSalesForReportData(listOrders, listReportItems);
-		
-		printReportData(listReportItems);
-		
-		return listReportItems;
-	}
-
-	private List<ReportItem> createReportData(Date startTime, Date endTime, String period) {
-		List<ReportItem> listReportItems = new ArrayList<>();
-		
-		Calendar startDate = Calendar.getInstance();
-		startDate.setTime(startTime);
-		
-		Calendar endDate = Calendar.getInstance();
-		endDate.setTime(endTime);	
-		
-		Date currentDate = startDate.getTime();
-		String dateString = dateFormatter.format(currentDate);
-		
-		listReportItems.add(new ReportItem(dateString));
-		
-		do {
-			if (period.equals("days")) {
-				startDate.add(Calendar.DAY_OF_MONTH, 1);
-			} else if (period.equals("months")) {
-				startDate.add(Calendar.MONTH, 1);
-			}
-			
-			currentDate = startDate.getTime();
-			dateString = dateFormatter.format(currentDate);	
-			
-			listReportItems.add(new ReportItem(dateString));
-			
-		} while (startDate.before(endDate));
-		
-		return listReportItems;		
-	}
-
-	public List<ReportItem> getReportDataLastYear() {
-		return getReportDataLastXMonths(12);
-	}
 }
--- BookstoreWeb/BookstoreAdmin/src/test/java/com/bookstore/admin/report/ReportRestControllerTest.java
+++ BookstoreWeb/BookstoreAdmin/src/test/java/com/bookstore/admin/report/ReportRestControllerTest.java
@@ -42,4 +42,20 @@ public class ReportRestControllerTest {
 		
 		mockMvc.perform(get(requestURL)).andExpect(status().isOk()).andDo(print());
 	}		
+
+	@Test
+	@WithMockUser(username = "user1", password = "pass1", authorities = {"Salesperson"})
+	public void testGetReportDataByCategory() throws Exception {
+		String requestURL = "/reports/category/last_7_days";
+		
+		mockMvc.perform(get(requestURL)).andExpect(status().isOk()).andDo(print());		
+	}
+
+	@Test
+	@WithMockUser(username = "user1", password = "pass1", authorities = {"Salesperson"})
+	public void testGetReportDataByProduct() throws Exception {
+		String requestURL = "/reports/product/last_7_days";
+		
+		mockMvc.perform(get(requestURL)).andExpect(status().isOk()).andDo(print());		
+	}
 }
