bb8e638860923e8920545509cd42b111b6edeab1
TDinhcuong81 <138292448+TDinhcuong81@users.noreply.github.com>
2024-08-26 13:52:03 +0700

    comment

--- BookstoreModel/src/main/java/com/bookstore/entity/Review.java
+++ BookstoreModel/src/main/java/com/bookstore/entity/Review.java
@@ -18,7 +18,7 @@ public class Review extends IdBasedEntity {
 	@Column(length = 128, nullable = false)
 	private String headline;
 	
-	@Column(length = 300, nullable = false)
+	@Column(length = 500, nullable = false)
 	private String comment;
 	
 	private int rating;	
